b261d4f12cb9b60c89b437ba8af3030319aa7f93
Sujit Simon <gsujitsimon@gmail.com>
2023-01-22 20:47:30 +0530

    PackageModification

---
+++ main.js
@@ -0,0 +1,5 @@
+import Clock from './component/Clock/Clock';
+import CountDownTimer from './component/Timer/Timer';
+import StopWatch from './component/StopWatch/StopWatch';
+
+export {Clock, CountDownTimer, StopWatch};
